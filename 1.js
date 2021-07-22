7b063795ef6092bf53c41094bea191db34df6fd6
AnyaKholodovich <77913661+AnyaKholodovich@users.noreply.github.com>
2021-07-22 22:08:48 +0300

    new files

--- 1.js
+++ 1.js
@@ -7,25 +7,33 @@
 // 	  уровень загрязнения каждой комнаты.
 
 
-function cleanRoom(dirtyLevel_1, dirtyLevel_2, dirtyLevel_3) {
+function cleanRoom(...dirtyLevel) {
     return new Promise(function(resolve, reject) {
       if ( dirtyLevel < 10) {
-        setTimeout(() => resolve(dirtyLevel_1), dirtyLevel_1 * 1000);
-        setTimeout(() => resolve(dirtyLevel_2), dirtyLevel_1 * 1000);
-        setTimeout(() => resolve(dirtyLevel_3), dirtyLevel_1 * 1000);
+        setTimeout(() => resolve(dirtyLevel), dirtyLevel * 1000);
       } else {
         setTimeout(() => reject('Ошибка!'), dirtyLevel);
       }
     });
 };
 
-cleanRoom(1, 2, 11).then(() =>{
-    return console.log(`Уборка проведена успешно за ${result} секунд`);
-}).then(() =>{
-    return console.log(`Уборка проведена успешно за ${result} секунд`);
-}).then(() =>{
-    return console.log(`Уборка проведена успешно за ${result} секунд`);
-})
-
-
-
+function cleanRooms(dirtyLevel_1, dirtyLevel_2, dirtyLevel_3) {
+  cleanRoom(dirtyLevel_1).then(
+    result => {
+      console.log(`Уборка комнаты №1 проведена успешно за ${result} секунд`)
+      return cleanRoom(dirtyLevel_2)
+    })
+    .then(
+      result => {
+        console.log(`Уборка комнаты №2 проведена успешно за ${result} секунд`)
+        return cleanRoom(dirtyLevel_3)
+      })
+    .then(
+      result => console.log(`Уборка комнаты №3 проведена успешно за ${result} секунд`))
+    .catch((error) => console.error(error));
+}
+  
+ 
+  
+cleanRooms(1, 10, 7);
+cleanRooms(1, 2, 3);
--- 2.js
+++ 2.js
@@ -6,4 +6,22 @@
 // 	  то пекращаем уборку полностью. Для этого функция cleanRooms(dirtyLevel_1, dirtyLevel_2, dirtyLevel_3) { ... } 
 // 	  должна принимать три аргемента, каждый из кторых описывает уровень загрязнения каждой комнаты.
 
+function cleanRoom(...dirtyLevel) {
+    return new Promise(function(resolve, reject) {
+      if ( dirtyLevel < 10) {
+        setTimeout(() => resolve(dirtyLevel), dirtyLevel * 1000);
+      } else {
+        setTimeout(() => reject('Ошибка!'), dirtyLevel);
+      }
+    });
+};
 
+function cleanRooms(dirtyLevel_1, dirtyLevel_2, dirtyLevel_3) {
+	let i = 1;
+	let arRooms = [cleanRoom(dirtyLevel_1), cleanRoom(dirtyLevel_2), cleanRoom(dirtyLevel_3)]
+	return Promise.all(arRooms).then(arRooms => arRooms.map(
+		result => console.log(`Уборка комнаты №${i++} проведена успешно за ${result} секунд`))).catch((err) => console.error(err))
+}
+
+cleanRooms(1, 3, 2);
+cleanRooms(1, 10, 2);
